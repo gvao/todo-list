b902f971c8ae47a48ff0637bac86e1f044eb6546
Yuri Galvao <yuri.gvao@gmail.com>
2023-04-05 15:15:30 -0300

    Inserting form logic

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
+
+formAddTodo.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const { target } = event
+    const inputValue = target.add.value.trim()
+
+    if (!inputValue) return
+
+    todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>`
+
+    target.reset()
+})
 
