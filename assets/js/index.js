3b4556a524e138cd93ec8c90a989662133047b9c
Yuri Galvao <yuri.gvao@gmail.com>
2023-07-06 11:57:57 -0300

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
-const todoList = document.querySelector('#todo-list')
-const form = document.querySelector('#form-search')
-const inputSearch = form.search
+const todoList = document.querySelector('#todo-list');
+const form = document.querySelector('#form-search');
+const inputSearch = form.search;
 
 const todos = [
     { id: newId(), title: 'Fazer isso', isDone: false },
@@ -8,75 +8,88 @@ const todos = [
     { id: newId(), title: 'Fazer tal coisa', isDone: true },
     { id: newId(), title: 'Almoçar', isDone: true },
     { id: newId(), title: 'Musculação', isDone: true },
-]
+];
 
 function newId(idLength = 26) {
-    const charset = 'abcdefghijklmnopqrstuwxyz' + 'ABCDEFGHIJKLMNOPQRSTUWXYZ' + '0123456789'
-    let id = ''
+    const charset = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
+    let id = '';
 
     for (let i = 0; i < idLength; i++) {
-        const randomLetter = Math.round(Math.random() * charset.length)
-
-        id += charset.charAt(randomLetter)
+        const randomLetter = Math.floor(Math.random() * charset.length);
+        id += charset.charAt(randomLetter);
     }
 
-    return id
+    return id;
 }
 
 const insertNewItemInTodo = (title, idInitial) => {
+    const id = idInitial || newId();
 
-    const id = idInitial || newId()
+    if (!title) return alert(`Inserir um valor válido`);
 
-    if (!title) return alert(`Inserir um valor válido`)
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.dataset.item = id;
 
-    todoList.innerHTML += `
-    <li class="todo-item" data-item="${id}">
-        <span class="todo-content">${title}</span>
-        <i class="fa fa-trash" data-trash="${id}"></i>
-    </li>`
-}
+    const span = document.createElement('span');
+    span.classList.add('todo-content');
+    span.textContent = title;
+
+    const trashIcon = document.createElement('i');
+    trashIcon.classList.add('fa', 'fa-trash');
+    trashIcon.dataset.trash = id;
+
+    li.appendChild(span);
+    li.appendChild(trashIcon);
+
+    todoList.appendChild(li);
+};
 
 const showItemIfMatchValue = value => {
-    const childrens = Array.from(todoList.children)
+    const childrens = Array.from(todoList.children);
 
     childrens.forEach(item => {
-        const textInLowerCase = item.textContent.toLowerCase()
-        const isMatch = textInLowerCase.includes(value.toLowerCase())
+        const textInLowerCase = item.textContent.toLowerCase();
+        const isMatch = textInLowerCase.includes(value.toLowerCase());
 
-        if (!isMatch) return item.classList.add('hidden')
-        return item.classList.remove('hidden')
-    })
-}
+        if (!isMatch) {
+            item.classList.add('hidden');
+        } else {
+            item.classList.remove('hidden');
+        }
+    });
+};
 
 const removeItem = event => {
-    const { target } = event
-    const idElement = target.getAttribute('data-trash')
-    
-    if (!idElement) return 
-    
-    const item = todoList.querySelector(`[data-item="${idElement}"]`)
-    item.remove()
-}
+    const { target } = event;
+    const idElement = target.getAttribute('data-trash');
 
-const handlerSubmit = event => {
-    event.preventDefault()
-    const title = inputSearch.value.trim()
+    if (!idElement) return;
 
-    insertNewItemInTodo(title)
+    const item = todoList.querySelector(`[data-item="${idElement}"]`);
+    item.remove();
+};
 
-    form.reset()
-    showItemIfMatchValue("")
-}
+const handleSubmit = event => {
+    event.preventDefault();
+    const title = inputSearch.value.trim();
 
-const handlerInput = event => {
-    const inputValue = event.target.value
+    insertNewItemInTodo(title);
 
-    showItemIfMatchValue(inputValue)
-}
+    form.reset();
+    showItemIfMatchValue("");
+};
 
-const insertInitialsTodos = todos
-    .forEach(({ title, id }) => insertNewItemInTodo(title, id))
+const handleInput = event => {
+    const inputValue = event.target.value;
 
-inputSearch.addEventListener('input', handlerInput)
-form.addEventListener('submit', handlerSubmit)
-todoList.addEventListener('click', removeItem)
+    showItemIfMatchValue(inputValue);
+};
+
+const insertInitialTodos = todos.forEach(({ title, id }) => {
+    insertNewItemInTodo(title, id);
+});
+
+inputSearch.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+todoList.addEventListener('click', removeItem);
