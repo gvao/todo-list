97ca5b183999ae3ce9dc82b466158e588df79642
Yuri Galvao <yuri.gvao@gmail.com>
2023-07-16 19:10:14 -0300

    Fix bug remove todo

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,3 +1,5 @@
+import { removeItem } from "./todos.js"
+
 const createElement = (tagName = '', classNames = [], onClick) => {
     const element = document.createElement(tagName)
     classNames.forEach(className => element.classList.add(className))
