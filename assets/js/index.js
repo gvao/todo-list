357ca4f5b028ac71c652c33aeda4b0c329cf7390
Yuri Galvao <yuri.gvao@gmail.com>
2023-07-16 17:51:50 -0300

    implement patterns

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,32 @@ const todoList = document.querySelector('#todo-list');
 const form = document.querySelector('#form-search');
 const inputSearch = form.search;
 
-
 const todos = [
-    { id: newId(), title: 'Fazer isso', isDone: false },
-    { id: newId(), title: 'Fazer aquilo', isDone: true },
-    { id: newId(), title: 'Fazer tal coisa', isDone: true },
-    { id: newId(), title: 'Almoçar', isDone: true },
-    { id: newId(), title: 'Musculação', isDone: true },
+    {
+        id: "DSE1r4RRBMcWJOpuHCWDwGu9h0",
+        title: "Fazer isso",
+        isDone: false
+    },
+    {
+        id: "QxJIPecBBjE93tX1bpsd59ZGS1",
+        title: "Fazer aquilo",
+        isDone: true
+    },
+    {
+        id: "GTdbU1tZ9ERoIi0b9UTM2j0r6y",
+        title: "Fazer tal coisa",
+        isDone: true
+    },
+    {
+        id: "DS6ILO1L4HeiPLpkEP4EKhbAAu",
+        title: "Almoçar",
+        isDone: false
+    },
+    {
+        id: "rAxi4PsuYYLTGxebm1X63UqGYw",
+        title: "Musculação",
+        isDone: true
+    }
 ];
 
 function newId(idLength = 26) {
@@ -23,43 +42,94 @@ function newId(idLength = 26) {
     return id;
 }
 
-const createElement = (tagName = '', classNames = []) => {
+function TodoFactory(title = '', isDone = false) {
+    const id = newId()
+
+    return {
+        id,
+        title,
+        isDone,
+    }
+}
+
+const createElement = (tagName = '', classNames = [], onClick) => {
     const element = document.createElement(tagName)
     classNames.forEach(className => element.classList.add(className))
 
+    if (onClick) {
+        element.addEventListener('click', onClick)
+    }
+
     return element
+}
+
+function renderTodos(todos = []) {
+    todoList.innerHTML = ''
+
+    todos
+        .forEach((todo) => {
+            const li = createElement('li', ['todo-item'])
+            li.dataset.item = todo.id
+
+            const span = createElement('span', ['todo-content'])
+            span.textContent = todo.title
+
+            const trash = createElement('i', ['fa', 'fa-trash'], () => removeItem(todo))
+            // trash.dataset.trash = id
+
+            li.appendChild(span)
+            li.appendChild(trash)
+
+            todoList.appendChild(li)
+        })
 }
 
 const addTodoItem = (title = '') => {
     if (!title) return alert('inserir um valor válido');
 
-    const newItem = {
-        id: newId(),
-        title,
-        isDone: false
-    }
+    const newItem = TodoFactory(title)
 
     todos.push(newItem)
-    renderTodos(todos)
+    notifyObservers(todos)
 }
 
-const removeItem = event => {
-    const { target } = event;
-    const idElement = target.getAttribute('data-trash');
+const removeItem = todo => {
+    const index = todos.indexOf(todo)
 
-    if (!idElement) return;
+    if (index < 0) return console.log(`Item not found in list`)
 
-    const item = todoList.querySelector(`[data-item="${idElement}"]`);
-    item.remove();
+    todos.splice(index, 1)
+    notifyObservers(todos)
 };
 
-const filterTodoList = searchValue => {
-    const valueLowerCase = searchValue.toLowerCase()
+const filterTodoList = (valueLowerCase) => {
+    if (!valueLowerCase) return todos
 
-    const filteredItems = todos.filter(({ title }) => title.toLowerCase().includes(valueLowerCase))
-
-    renderTodos(filteredItems)
+    return todos.filter(({ title }) => title.toLowerCase().includes(valueLowerCase))
 };
+
+const { addObserver, removeObserver, notifyObservers } = (() => {
+    const observers = []
+
+    const addObserver = observer => observers.push(observer)
+
+    const removeObserver = observer => {
+        const index = observers.indexOf(observer)
+        if (index < 0) return console.log('observer not found')
+
+        observers.splice(index, 1)
+    }
+
+    const notifyObservers = (todos) => observers.forEach(observer => observer(todos))
+
+    return {
+        addObserver,
+        removeObserver,
+        notifyObservers,
+    }
+})()
+
+addObserver(renderTodos)
 
 const handleSubmit = event => {
     event.preventDefault();
@@ -67,38 +137,17 @@ const handleSubmit = event => {
 
     addTodoItem(title);
     form.reset();
+    notifyObservers(todos)
 };
 
 function handleInput(event) {
     const inputValue = event.target.value;
 
-    filterTodoList(inputValue);
+    const todosFiltered = filterTodoList(inputValue);
+    notifyObservers(todosFiltered)
 };
-
-function renderTodos(todos = []) {
-    todoList.innerHTML = ''
-
-    todos
-        .forEach(({ id, title }) => {
-            const li = createElement('li', ['todo-item'])
-            li.dataset.item = id
-
-            const span = createElement('span', ['todo-content'])
-            span.textContent = title
-
-            const trash = createElement('i', ['fa', 'fa-trash'])
-            trash.dataset.trash = id
-
-            li.appendChild(span)
-            li.appendChild(trash)
-
-            todoList.appendChild(li)
-        })
-
-}
 
 form.addEventListener('submit', handleSubmit);
 inputSearch.addEventListener('input', handleInput);
-todoList.addEventListener('click', removeItem);
 
 renderTodos(todos)
