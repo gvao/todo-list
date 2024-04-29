f06477976a06c800f2de34dbd5bf3454f04984eb
Yuri Galvao <yuri.gvao@gmail.com>
2024-04-29 04:34:21 -0300

    created test Todo entity

---
+++ test/domain/Todo.test.js
@@ -0,0 +1,27 @@
+import { describe, it } from "node:test";
+import { strictEqual, deepEqual } from "node:assert/strict"
+import Todo from "../../src/core/domain/Todo.js";
+
+describe('Todo', () => {
+    it('should create a new Todo', () => {
+        const todo = Todo.create('any_title')
+        console.log(`>>>>>`, todo);
+        strictEqual(todo.title, 'any_title')
+    })
+
+    it('should change status todo', () => {
+        const todo = Todo.create('any_title')
+        todo.done()
+        strictEqual(todo.isDone, true)
+    })
+
+    it('should return dto', () => {
+        const todo = Todo.create('any_title')
+        const { id, ...dto } = todo.dto
+        deepEqual(dto, {
+            title: 'any_title',
+            isDone: false,
+        })
+    })
+
+})
