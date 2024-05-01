ec728f0e7e1130fe54e7d5fb17ff4ae7343dda22
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-01 11:04:58 -0300

    Change responsibility to app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import path from 'node:path'
 
 import AddTodo from "./core/application/useCases/AddTodo.js";
 import GetAllTodo from "./core/application/useCases/GetAllTodo.js";
-import Route from "./core/domain/Route.js";
 import Routes from "./core/domain/Routes.js";
 import TodoRepositoryInMemory from "./core/infra/TodoRepositoryInMemory.js";
 import Server from './core/domain/Server.js';
@@ -12,15 +11,18 @@ import AddTodoController from './core/infra/controllers/AddTodoController.js';
 const todoRepository = new TodoRepositoryInMemory()
 const getAllTodo = new GetAllTodo(todoRepository)
 const addTodo = new AddTodo(todoRepository)
-const routes = new Routes()
-
-const publicPath = path.join('src', 'public')
-await routes.usePublic(publicPath)
 
 const addTodoController = new AddTodoController(addTodo)
 const getTodoListController = new GetTodoListController(getAllTodo)
 
-routes.addRoute(getTodoListController.controller())
-routes.addRoute(addTodoController.controller())
+const routes = new Routes()
+const app = new Server(routes)
 
-export const app = new Server(routes)
+const publicPath = path.join('src', 'public')
+
+app.staticPath(publicPath)
+app.addRoute(getTodoListController.controller())
+app.addRoute(addTodoController.controller())
+
+
+export { app }
