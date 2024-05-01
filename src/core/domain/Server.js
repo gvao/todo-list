ba9fc68c9341590bdb9c948e4f4591c7bc1aa529
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-01 10:08:06 -0300

    move createServer to listen method

--- src/core/domain/Server.js
+++ src/core/domain/Server.js
@@ -1,22 +1,33 @@
 import http from 'node:http'
 import Routes from './Routes.js'
+import Route from './Route.js'
 
 export default class Server {
-    #server
+    /** @type {Routes} */
+    #routes
 
     /** @param {Routes} routes  */
     constructor(routes) {
-        this.#server = http.createServer(async function (req, res) {
-            const url = new URL(req.url, `http://${req.headers.host}`);
-            const route = routes.getRoute(url, req.method)
-            if(!!route) return await route.handler(req, res)
-            
-            res.statusCode = 404
-            
-        })
+        this.routes = routes
     }
 
-    listen(port, callback) {
-        return this.#server.listen(port, callback)
+    /** @param {Route} route */
+    addRoute = (route) => { this.#routes.addRoute(route) }
+    /** @param {string} path  */
+    staticPath = async (path) => { this.#routes.usePublic(path) }
+    /** @param {any} middleware */
+    use = (middleware) => { this.middleware.push(middleware) }
+    /** @param {number} port @param {() => void} callback */
+    listen = (port, callback) => {
+        const routes = this.#routes
+
+        const server = http.createServer(async function (req, res) {
+            const url = new URL(req.url, `http://${req.headers.host}`);
+            const route = routes.getRoute(url, req.method)
+            if (!!route) return await route.handler(req, res)
+            res.statusCode = 404
+        })
+
+        return server.listen(port, callback)
     }
 }
