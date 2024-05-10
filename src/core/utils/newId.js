eace958dfd66c8f9b78ce71824cf12ae4fcddca0
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-10 01:41:06 -0300

    Fiz module.exports newId

--- src/core/utils/newId.js
+++ src/core/utils/newId.js
@@ -1,11 +1,13 @@
-export function newId(idLength = 26) {
-    const charset = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
-    let id = '';
+module.exports = {
+    newId(idLength = 26) {
+        const charset = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
+        let id = '';
 
-    for (let i = 0; i < idLength; i++) {
-        const randomLetter = Math.floor(Math.random() * charset.length);
-        id += charset.charAt(randomLetter);
+        for (let i = 0; i < idLength; i++) {
+            const randomLetter = Math.floor(Math.random() * charset.length);
+            id += charset.charAt(randomLetter);
+        }
+
+        return id;
     }
-
-    return id;
 }
