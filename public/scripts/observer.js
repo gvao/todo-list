f828f42efe0e71da9e06ea4ab3e49316d0e69576
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-13 10:54:03 -0300

    separate types

--- public/scripts/observer.js
+++ public/scripts/observer.js
@@ -14,18 +14,3 @@ class Observer {
     
 }
 
-/**
- * @typedef {object} Todo
- * @property {string} id
- * @property {string} title
- * @property {boolean} isDone
- */
-
-
-/**
-* @typedef {Object} EventDomain
-* @property {string} type
-* @property {string} id
-* @property {boolean} status
-* @property {string} input
-*/
---
+++ public/scripts/types.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef {object} Todo
+ * @property {string} id
+ * @property {string} title
+ * @property {boolean} isDone
+ */
+
+
+/**
+* @typedef {Object} EventDomain
+* @property {string} type
+* @property {string} id
+* @property {boolean} status
+* @property {string} input
+*/
