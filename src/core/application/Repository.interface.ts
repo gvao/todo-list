3f7784868c4f919b6068d564f7df5ddba5d684a9
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-16 18:37:45 -0300

    Segregate repositories methods

--- src/core/application/Repository.interface.ts
+++ src/core/application/Repository.interface.ts
@@ -1,6 +1,19 @@
-export default interface Repository<T> {
+export interface SaveRepository<T> {
     save(item: T): void
+}
+
+export interface GetAllRepository<T> {
     getAll(): T[]
+}
+
+export interface GetByIdRepository<T> {
     getById(id: string): T | undefined
+}
+
+export interface DeleteByIdRepository<T> {
     deleteById(id: string): void
 }
+
+type Repository<T> = SaveRepository<T> & GetAllRepository<T> & GetByIdRepository<T> & DeleteByIdRepository<T>
+
+export default Repository
