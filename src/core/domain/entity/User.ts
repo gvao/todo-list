757a92c78f3524f60f8010162a6e196e39c1a030
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-17 11:21:13 -0300

    created User entity

---
+++ src/core/domain/entity/User.ts
@@ -0,0 +1,36 @@
+import Password from "../Password"
+
+export default class User implements UserInterface {
+    id: string
+    username
+    private password
+    constructor({ username, password, id }: UserProps) {
+        this.username = username
+        this.password = password
+        this.id = id
+    }
+
+    passwordValidate(plainPassword: string): boolean {
+        return this.password.validate(plainPassword)
+    }
+
+    static create({ username, password: plainPassword }: UserCreateProps) {
+        const id = crypto.randomUUID()
+        const salt = 'salt'
+        const password = Password.create(plainPassword, salt)
+        return new User({ username, password, id });
+    }
+}
+
+
+interface UserInterface {
+    id: string
+    username: string
+    passwordValidate(plainPassword: string): boolean
+}
+
+type UserProps = Pick<UserInterface, 'username' | 'id'> & {
+    password: Password
+}
+
+type UserCreateProps = Pick<UserInterface, "username"> & { password: string }
---
+++ test/domain/User.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from "vitest";
+import User from '../../src/core/domain/entity/User'
+
+describe('User', () => {
+    const input = {
+        username: 'any_username',
+        password: 'any_password'
+    }
+    const user = User.create(input)
+    it('create user', () => {
+        expect(user.id).toBeDefined();
+        expect(user.username).toBe('any_username');
+    })
+    describe('#password', () => {
+        it('validate password', () => {
+            const result = user.passwordValidate(input.password)
+            expect(result).toBeTruthy()
+        })
+        it('invalid password', () => {
+            const result = user.passwordValidate('invalid_password')
+            expect(result).toBeFalsy()
+        })
+    })
+})
