2ffa3fb302246726b1698f6e1de9ed81763d81ec
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-19 11:04:00 -0300

    test cadaster username existed

--- src/application/useCases/Signup.ts
+++ src/application/useCases/Signup.ts
@@ -1,17 +1,19 @@
 import User from "../../domain/entity/User"
-import { SaveRepository } from "../Repository.interface"
+import { GetByUsernameRepository, SaveRepository } from "../Repository.interface"
 
 export default class Signup {
 
     constructor(private userRepository: UserRepository) {}
 
     async execute({ username, password }: SignupInput): Promise<void> {
+        const userExisting = await this.userRepository.getByUsername(username)
+        if(!!userExisting) throw new Error('user already exists')
         const user = User.create({ username, password })
         await this.userRepository.save(user)
     }
 }
 
-type UserRepository = SaveRepository<User>
+type UserRepository = SaveRepository<User> & GetByUsernameRepository<User>
 
 type SignupInput = {
     username: string
--- test/application/useCases/Signup.test.ts
+++ test/application/useCases/Signup.test.ts
@@ -3,14 +3,17 @@ import Signup from '../../../src/application/useCases/Signup'
 import UserRepositoryInMemory from '../../../src/infra/repositories/UserRepositoryInMemory'
 
 describe('Signup', () => {
+    const input = { username: 'any_user', password: 'any_password' }
+    const userRepository = new UserRepositoryInMemory()
+    const signup = new Signup(userRepository)
     it('cadaster new user', async () => {
-        const userRepository = new UserRepositoryInMemory()
-        const signup = new Signup(userRepository)
-        const input = { username: 'any_user', password: 'any_password' }
         await signup.execute(input)
-        const {users} = userRepository
+        const { users } = userRepository
         expect(users).toHaveLength(1)
         expect(users[0].username).toBe('any_user')
+    })
+    it('should not cadaster user existing', async () => {
+        expect(() => signup.execute(input)).rejects.toThrow('user already exists')
     })
 })
 
