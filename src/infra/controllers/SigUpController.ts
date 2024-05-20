c14d7410641d4f8b0b5f6430707c3d2143903a6d
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-20 09:29:43 -0300

    created SignUpController

---
+++ src/infra/controllers/SigUpController.ts
@@ -0,0 +1,21 @@
+import Signup from "../../application/useCases/Signup";
+import Route from "../Route";
+
+export default class SigUpController {
+    constructor(private signup: Signup){}
+    controller = () => new Route('POST', '/signup', async (req, res) => {
+        try {
+            const { body } = req
+            const { username, password } = body
+            await this.signup.execute({ username, password })
+            res.statusCode = 201
+            res.end()
+        } catch(err) {
+            const error = err as Error
+            if(error.message ===  'user already exists') {
+                res.statusCode = 304
+                res.end()
+            }
+        }
+    })
+}
---
+++ test/infra/SigupController.test.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import SigUpController from '../../src/infra/controllers/SigUpController'
+import Route from '../../src/infra/Route'
+import Signup from '../../src/application/useCases/Signup'
+import UserRepositoryInMemory from '../../src/infra/repositories/UserRepositoryInMemory'
+
+describe('SigUpController', () => {
+    const userRepository = new UserRepositoryInMemory()
+    let signup: Signup
+    let sigUpController: SigUpController
+    let responseMock: { end: () => void, statusCode: number }
+
+    beforeEach(() => {
+        signup = new Signup(userRepository)
+        sigUpController = new SigUpController(signup)
+        responseMock = { end: () => { }, statusCode: 0 }
+    })
+
+    it('should cadaster new user in repository', async () => {
+        const body = { username: 'any_username', password: 'any_password' }
+        const route = sigUpController.controller()
+        expect(route).toBeInstanceOf(Route)
+        await route.handler({ body }, responseMock)
+        expect(responseMock.statusCode).toBe(201)
+        expect(userRepository.users).toHaveLength(1)
+    })
+    it('should return error if user existing', async () => {
+        const body = { username: 'any_username', password: 'any_password' }
+        const route = sigUpController.controller()
+        await route.handler({ body }, responseMock)
+        expect(responseMock.statusCode).toBe(304)
+        expect(userRepository.users).toHaveLength(1)
+    })
+})
