4c8d2a231b0e7749dea512de0c38e298bd8e0ad3
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-22 11:46:54 -0300

    required status method em type

--- src/infra/types.ts
+++ src/infra/types.ts
@@ -4,5 +4,5 @@ import http from 'node:http'
 export type Middleware = (req: Request, res: Response, next?: () => void) => void
 export type Request = http.IncomingMessage & { params?: any, body?: any }
 export type Response = http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage } & StatusInterface
-export type StatusInterface = { status?: (status: number) => { json: (data: any) => void } }
+export type StatusInterface = { status: (status: number) => { json: (data?: any) => void } }
 export type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
