9c0fb4efda6c6c64a46b8dfda3f1abbbf39f7569
Yuri Galvao <yuri.gvao@gmail.com>
2024-05-29 01:37:51 -0300

    remove fetcher in file

--- public/scripts/todoGateway.js
+++ public/scripts/todoGateway.js
@@ -19,15 +19,3 @@ class TodoGateway {
         fetcher(`/api/todos/${id}/changeStatus`, 'POST', { status })
     }
 }
-
-async function fetcher(url, method = 'GET', body) {
-    const options = {
-        method,
-    }
-    if (method !== 'GET' && !!body) {
-        options.headers = { 'Content-Type': 'application/json' }
-        options.body = JSON.stringify(body)
-    }
-
-    return fetch(url, options)
-}
