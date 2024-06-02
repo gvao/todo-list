6b4d15fa8ec9cdf8a81dbc83ab4ee716a1ee3548
Yuri Galvao <yuri.gvao@gmail.com>
2024-06-02 17:56:00 -0300

    add auth parameter

--- public/scripts/utils/fetcher.js
+++ public/scripts/utils/fetcher.js
@@ -2,14 +2,18 @@
  * @param {string} url 
  * @param {'GET' | 'POST'| 'DELETE'} method 
  * @param {any} body 
+ * @param {string} token
  */
-async function fetcher(url, method = 'GET', body) {
+async function fetcher(url, method = 'GET', body, auth) {
     const options = {
         method,
     }
     if (method !== 'GET' && !!body) {
         options.headers = { 'Content-Type': 'application/json' }
         options.body = JSON.stringify(body)
+    }
+    if(auth) {
+        options.headers['authorization'] = auth
     }
 
     return fetch(url, options)
