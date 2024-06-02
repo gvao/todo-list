4925ce3a7f4e9f06c6ce54f1e8f9367a05ff8f26
Yuri Galvao <yuri.gvao@gmail.com>
2024-06-02 17:26:29 -0300

    updated create user todo

--- src/todoContext/infra/controllers/CreateUserTodoController.ts
+++ src/todoContext/infra/controllers/CreateUserTodoController.ts
@@ -1,7 +1,6 @@
 import { GetByUsernameRepository } from "../../../authContext/application/Repository.interface";
 import TokenGenerate from "../../../authContext/domain/service/TokenGenerate";
 import Route from "../../../shared/domain/Route";
-import { getToken } from "../../../shared/utils/getToken";
 import CreateUserTodo from "../../application/useCases/CreateUserTodo";
 import Controller from "./interface";
 
@@ -9,22 +8,11 @@ export default class CreateUserTodoController implements Controller {
     constructor(private createUserTodo: CreateUserTodo, private tokenGenerate: TokenGenerate, private userRepository: GetByUsernameRepository ) { }
     controller() {
         return new Route('POST', '/api/users/todo', async (req, res) => {
-
-
             const {
                 method,
-                body: { title },
-                headers: { authorization },
+                body: { title, id: userId },
             } = req
-
-            if (!authorization) throw new Error(`Invalid authorization`)
-            const token = getToken(authorization)
-            token
-            const username = this.tokenGenerate.verify(token) as string
-            const user = await this.userRepository.getByUsername(username)
-            if (!user) return null
-            
-            await this.createUserTodo.execute({ title, userId: user.id })
+            await this.createUserTodo.execute({ title, userId })
             res.writeHead(201, { "Content-Type": "application/json" })
             res.end(JSON.stringify({ message: `Todo created!` }))
         })
