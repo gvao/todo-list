6ab2fdc85b277617a1ea302f939b63db222ae200
yuri.gvao <yuri.gvao@gmail.com>
2024-06-06 15:19:56 +0000

    refactored CreateUserTodoController

--- src/todoContext/infra/controllers/CreateUserTodoController.ts
+++ src/todoContext/infra/controllers/CreateUserTodoController.ts
@@ -1,20 +1,14 @@
-import { GetByUsernameRepository } from "../../../authContext/application/Repository.interface";
-import TokenGenerate from "../../../authContext/domain/service/TokenGenerate";
 import Route from "../../../shared/domain/Route";
 import CreateUserTodo from "../../application/useCases/CreateUserTodo";
 import Controller from "./interface";
 
 export default class CreateUserTodoController implements Controller {
-    constructor(private createUserTodo: CreateUserTodo, private tokenGenerate: TokenGenerate, private userRepository: GetByUsernameRepository ) { }
+    constructor(private createUserTodo: CreateUserTodo) { }
     controller() {
-        return new Route('POST', '/api/users/todo', async (req, res) => {
-            const {
-                method,
-                body: { title, id: userId },
-            } = req
+        return new Route('POST', '/api/user/todo', async (req, res) => {
+            const { body: { title, id: userId }, } = req
             await this.createUserTodo.execute({ title, userId })
-            res.writeHead(201, { "Content-Type": "application/json" })
-            res.end(JSON.stringify({ message: `Todo created!` }))
+            res.status!(201).json({ message: `Todo created!` })
         })
     }
 
