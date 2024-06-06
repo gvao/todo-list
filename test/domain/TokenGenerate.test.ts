ad25a027d63a1cf70da58343c3a2a333f37371f6
yuri.gvao <yuri.gvao@gmail.com>
2024-06-06 13:41:20 +0000

    created verify test

--- test/domain/TokenGenerate.test.ts
+++ test/domain/TokenGenerate.test.ts
@@ -2,10 +2,27 @@ import { describe, expect, it } from "vitest";
 import TokenGenerate from "../../src/authContext/domain/service/TokenGenerate"
 
 describe('TokenGenerate', () => {
-    it('shoul return token', () => {
-        const expectedToken = 'eyJhbGciOiJIUzI1NiJ9.YW55X3VzZXJuYW1l.2LGxQDd1Jzf3A_aKv01GxcsoXySCODioasWfR53y82k'
-        const tokenGenerate = new TokenGenerate('secret')
-        const token = tokenGenerate.generate('any_username')
-        expect(token).toBe(expectedToken)
+    const tokenGenerate = new TokenGenerate('secret')
+    const expectedToken = 'eyJhbGciOiJIUzI1NiJ9.YW55X3VzZXJuYW1l.2LGxQDd1Jzf3A_aKv01GxcsoXySCODioasWfR53y82k'
+
+    describe('#generate', () => {
+        it('shoul return token', () => {
+            const token = tokenGenerate.generate('any_username')
+            expect(token).toBe(expectedToken)
+        })
     })
+
+    describe('#verify', () => {
+        it('Valid token', async () => {
+            const payload = tokenGenerate.verify(expectedToken)
+            expect(payload).toBe('any_username')
+        })
+
+        it('should return error with invalid parameters', async () => {
+            const invalidToken = ['vckbdf', '']
+            for (const token of invalidToken)
+                expect(() => { tokenGenerate.verify(token) }).toThrowError()
+        })
+    })
+
 })
