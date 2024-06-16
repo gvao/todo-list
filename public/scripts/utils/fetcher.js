d0c83e7d5e7e3d71163f8b8970b226121f3fdacd
Yuri Galvao <yuri.gvao@gmail.com>
2024-06-16 12:52:22 -0300

    updated fetcher

--- public/scripts/utils/fetcher.js
+++ public/scripts/utils/fetcher.js
@@ -2,15 +2,17 @@
  * @param {string} url 
  * @param {'GET' | 'POST'| 'DELETE'} method 
  * @param {any} body 
+ * @param {Partial<RequestInit>} optionsInit 
  */
-async function fetcher(url, method = 'GET', body) {
+async function fetcher(url, method = 'GET', body, optionsInit) {
+    /** @type {RequestInit} */
     const options = {
         method,
+        headers: optionsInit?.headers || {}
     }
     if (method !== 'GET' && !!body) {
-        options.headers = { 'Content-Type': 'application/json' }
+        options.headers = { ...options.headers, 'Content-Type': 'application/json' }
         options.body = JSON.stringify(body)
     }
-
     return fetch(url, options)
 }
